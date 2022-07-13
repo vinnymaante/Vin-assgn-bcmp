81cdbd2d6aa2e1250bc5086c975ee3803305e997
Vincent Maante <107935106+vinnymaante@users.noreply.github.com>
2022-07-13 15:15:36 +0300

    Delete vin assgn bcmp directory

--- vin assgn bcmp/assets/main.js
+++
@@ -1,8 +0,0 @@
-const btn = document .createElement("button");
-btn.innerHTML = "Click me";
-btn.addEventListener("click",() =>{
-    alert("Hello Vincent Maante, welcome to Bootcam"
-    )
-
-});
-document.body.appendChild(btn);
