adfc3ee8a4e838c49f56d0c4492d00d720b0083a
Vincent Maante <107935106+vinnymaante@users.noreply.github.com>
2022-07-13 15:11:31 +0300

    Add files via upload

---
+++ vin assgn bcmp/assets/main.js
@@ -0,0 +1,8 @@
+const btn = document .createElement("button");
+btn.innerHTML = "Click me";
+btn.addEventListener("click",() =>{
+    alert("Hello Vincent Maante, welcome to Bootcam"
+    )
+
+});
+document.body.appendChild(btn);
